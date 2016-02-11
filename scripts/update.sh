39891f73d02ce5c2fc0434874fc8924b9ecde0c1
Oliver Beckstein <orbeckst@gmail.com>
2016-02-11 01:11:59 -0700

    fixed update.sh script
    - closes #2 and ASU-CompMethodsPhysics-PHY494/PHY494-resources#2
    - add skeleton as a remote (only once)

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,6 +5,8 @@
 # Oliver Beckstein 2016, placed into the public domain
 
 progname="$0"
+REMOTE_NAME="skeleton"
+REMOTE_URL="https://github.com/ASU-CompMethodsPhysics-PHY494/PHY494-assignments-skeleton.git"
 
 function die () {
     local msg="$1" err=${2:-1}
@@ -12,8 +14,19 @@ function die () {
     exit $err
 }
 
-echo "updating repository... git pull"
-git pull
+function set_remote () {
+    local NAME="$1" URL="$2"
+    if ! git remote get-url ${NAME} >/dev/null 2>&1; then
+	echo "Adding remote repository '${NAME}'."
+	git remote add ${NAME} ${URL}
+    fi
+}
+
+set_remote ${REMOTE_NAME} ${REMOTE_URL}
+    
+
+echo "updating repository... git pull from ${REMOTE_NAME}"
+git pull ${REMOTE_NAME} master
 
 topdir="$(git rev-parse --show-toplevel)" || die "Failed to get rootdir"
 cd "${topdir}" || die "Failed to get to the git root dir ${rootdir}"
