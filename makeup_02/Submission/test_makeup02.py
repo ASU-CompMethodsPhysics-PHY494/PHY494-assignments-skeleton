24c8227cd089f0435325af3f21dd2ade699121c2
Oliver Beckstein <orbeckst@gmail.com>
2016-05-05 00:46:36 -0700

    Makeup02:
    These corrections were committed AFTER the submission deadline for students.
    During grading, students should not have any disadvantages due to these
    changes.
    
    - tests for wrong initial conditions (issue ASU-CompMethodsPhysics-PHY494/PHY494-assignments-skeleton#5 )
      - the problem PDF states one set of initial conditions but in the solution
        I implemented another (see issue ASU-CompMethodsPhysics-PHY494/PHY494-assignments-skeleton#5
        for details). The tests probed the
        "wrong" initial conditions. I added a test that will alert if someone
        implemented the "correct" initial conditions (which will, however, make
        other tests fail)
      - added a flag to my solution to switch between initial conditions
    - changed PDF text to implement boundary conditions of the solution (so they
      are now the "correct" ic)
    - add a second test class for Crank-Nicholson to catch the case when step and Dt were
      confused (unfortunately, in the initial test set, I had set both to 30 and
      hence did not catch certain errors)

--- makeup_02/Submission/test_makeup02.py
+++ makeup_02/Submission/test_makeup02.py
@@ -38,15 +38,40 @@ class TestCrankNicholson_T(object):
         self.t_max = 3*24*3600
         self.Tmin = 301.4
         self.Tmax = 314.1
-        self.T_plot, self.parameters = makeup02.CrankNicholson_T(
-            Dx=self.Dx, Dt=self.Dt, step=self.step,
-            Tin=293, Tmin=self.Tmin, Tmax=self.Tmax,
-            L=0.3, t_max=self.t_max,
-            Kappa=1.0, CHeat=0.9e3, rho=2e3)
+        self.Tin = 293
+        self.length = 0.3
         self.figname = "heatequation_3d.pdf"
+        self.T_plot, self.parameters = self.CrankNicholson_T()
+
+    def CrankNicholson_T(self):
+        return makeup02.CrankNicholson_T(
+            Dx=self.Dx, Dt=self.Dt, step=self.step,
+            Tin=self.Tin, Tmin=self.Tmin, Tmax=self.Tmax,
+            L=self.length, t_max=self.t_max,
+            Kappa=1.0, CHeat=0.9e3, rho=2e3)
 
     def test_parameters(self):
         assert_array_almost_equal(self.parameters, (0.005, 30, 30))
+
+    def test_initial_conditions_as_implemented(self):
+        T = np.empty_like(self.T_plot[0])
+        T[1:-1] = Tin
+        T[0] = makeup02.T_diurnal(0, Tmin, Tmax)
+        T[-1] = Tin
+        assert_array_almost_equal(self.T_plot[0], T)
+
+    def test_initial_conditions_as_in_PDF(self):
+        T = np.empty_like(self.T_plot[0])
+        T[:] = Tmin
+        if np.allclose(self.T_plot[0], T):
+            import textwrap
+            raise AssertionError(textwrap.fill(
+                "In principle you implemented the correct initial conditions "
+                "as required in the PDF. However, the tests were written for "
+                "slightly different initial conditions: see issue #5"
+                "https://github.com/ASU-CompMethodsPhysics-PHY494/PHY494-assignments-skeleton/issues/5."
+                "Please implement the alternative initial conditions as provided in the "
+                "link above."))
 
     def test_left_boundary(self):
         Nt = int(self.t_max // self.Dt)
@@ -78,3 +103,30 @@ class TestCrankNicholson_T(object):
         assert_(os.path.exists(self.figname),
                 "Figure {} is missing.".format(self.figname))
 
+
+class TestCrankNicholson_T_2(TestCrankNicholson_T):
+    # Better test where step and Dt are not the same number (catches
+    # cases where step was used instead of Dt)
+    # NOTE: These tests were added AFTER the submission deadline to
+    #       aid in grading.
+    def setUp(self):
+        self.Dx = 0.005
+        self.Dt = 100
+        self.step = 30
+        self.t_max = 3*24*3600
+        self.Tmin = 301.4
+        self.Tmax = 314.1
+        self.Tin = 293
+        self.length = 0.3
+        self.figname = "heatequation_3d.pdf"
+        self.T_plot, self.parameters = self.CrankNicholson_T()
+
+    def test_parameters(self):
+        assert_array_almost_equal(self.parameters, (0.005, 100, 30))
+
+    def test_lattice_size(self):
+        assert_array_equal(self.T_plot.shape, (88, 59))
+
+    def test_solution(self):
+        assert_almost_equal(self.T_plot.sum(), 1557760.8276307792,
+                            decimal=4)
