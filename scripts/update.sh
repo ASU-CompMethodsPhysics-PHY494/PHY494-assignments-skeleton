94bc9a42ad89c09c9371f837f360922f510b7394
Oliver Beckstein <orbeckst@gmail.com>
2017-01-28 03:48:10 -0700

    update script also touches project_[1-9]

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 # Update the PHY 494 Assignment Repository with new
 # homeworks and data.
 #
-# Oliver Beckstein 2016, placed into the public domain
+# Oliver Beckstein 2016,2017, placed into the public domain
 
 progname="$0"
 REMOTE_NAME="skeleton"
@@ -32,7 +32,7 @@ topdir="$(git rev-parse --show-toplevel)" || die "Failed to get rootdir"
 cd "${topdir}" || die "Failed to get to the git root dir ${rootdir}"
 
 echo "creating subdirectories (if any are missing)"
-for adir in assignment_[0-9][0-9]; do
+for adir in assignment_[0-9][0-9] project_[1-9]; do
     for subdir in Grade Submission Work; do
 	newdir="${adir}/${subdir}"
 	test -d "${newdir}" || \
