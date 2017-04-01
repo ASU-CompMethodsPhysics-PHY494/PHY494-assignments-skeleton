43d7ecd2b7a5c928e7dc51a5e1ca3882de06175a
Oliver Beckstein <orbeckst@gmail.com>
2017-04-01 11:56:10 -0700

    HW07: Newton-Raphson: square root, plate capacitor

---
+++ assignment_07/Submission/test_capacitor.py
@@ -0,0 +1,80 @@
+# tests for HW07 solution
+
+import numpy as np
+
+from numpy.testing import (assert_array_almost_equal,
+                           assert_array_equal,
+                           assert_equal,
+                           assert_almost_equal)
+
+
+def test_import_module():
+    try:
+        import capacitor
+    except ImportError:
+        raise AssertionError("Cannot import capacitor.py module")
+
+def test_import_function():
+    try:
+        from capacitor import calculate_phi_capacitor
+    except ImportError:
+        raise AssertionError("Cannot import the capacitor.calculate_phi_capacitor function")
+
+
+def test_run_long(Max_iter=10000, tol=1e-3):
+    import capacitor
+
+    Phi = capacitor.calculate_phi_capacitor(Max_iter=Max_iter, tol=tol)
+    assert_almost_equal(Phi.min(), -100)
+    assert_almost_equal(Phi.max(), 100)
+    assert_almost_equal(np.trace(Phi), 47.361932522707932, decimal=3)
+
+def test_run_short(Max_iter=1000, tol=1):
+    import capacitor
+
+    Phi = capacitor.calculate_phi_capacitor(Max_iter=Max_iter, tol=tol)
+    assert_almost_equal(Phi.min(), -100)
+    assert_almost_equal(Phi.max(), 100)
+    assert_almost_equal(np.trace(Phi), 29.296772764719591, decimal=0)
+
+class TestBoundaryConditions(object):
+    def setup(self):
+        import capacitor
+
+        self.Nmax = 100
+        self.Phi0 = np.zeros((self.Nmax, self.Nmax))
+        self.Phi0 = capacitor.set_boundaries(self.Phi0)
+        # correct values for the given problem
+        self.plate_size = 50
+        self.voltage = 100.
+
+    def test_voltage(self):
+        assert_almost_equal(self.Phi0.min(), -self.voltage)
+        assert_almost_equal(self.Phi0.max(), self.voltage)
+
+    def test_plate_size(self):
+        plus_plate = self.Phi0 > 0
+        minus_plate = self.Phi0 < 0
+        assert_equal(np.sum(plus_plate), self.plate_size)
+        assert_equal(np.sum(minus_plate), self.plate_size)
+
+    def test_plus_plate_position(self):
+        x, y = np.where(self.Phi0 > 0)
+        assert_equal(x.min(), 25)
+        assert_equal(y.min(), 40)
+        assert_equal(x.max(), 74)
+        assert_equal(y.max(), 40)
+
+    def test_minus_plate_position(self):
+        x, y = np.where(self.Phi0 < 0)
+        assert_equal(x.min(), 25)
+        assert_equal(y.min(), 60)
+        assert_equal(x.max(), 74)
+        assert_equal(y.max(), 60)
+
+    def test_box_boundary(self):
+        assert_array_equal(self.Phi0[:, 0], 0)
+        assert_array_equal(self.Phi0[:, -1], 0)
+        assert_array_equal(self.Phi0[0, :], 0)
+        assert_array_equal(self.Phi0[-1, :], 0)
+
---
+++ assignment_07/Submission/test_functions.py
@@ -0,0 +1,35 @@
+# tests for HW07 solution
+
+
+import pytest
+
+import numpy as np
+
+from numpy.testing import (assert_array_almost_equal,
+                           assert_array_equal,
+                           assert_equal,
+                           assert_almost_equal)
+
+
+def test_import_module():
+    try:
+        import functions
+    except ImportError:
+        raise AssertionError("Cannot import functions.py module")
+
+def test_import_function():
+    try:
+        from functions import sqrt
+    except ImportError:
+        raise AssertionError("Cannot import the functions.sqrt function")
+
+@pytest.mark.parametrize('decimals', [6, 4, 3, 8, 12])
+@pytest.mark.parametrize('x',
+                         [0, pytest.mark.xfail(0.456e-8),
+                          pytest.mark.xfail(1e-3), 0.1, 0.64, 0.99,
+                          1, 5, 9, 12.5, 1e3, 1.2345e8])
+def test_sqrt(x, decimals):
+    import functions
+    tol = 10**(-decimals)
+    assert_almost_equal(functions.sqrt(x, tol=tol), np.sqrt(x), decimal=decimals-1)
+
