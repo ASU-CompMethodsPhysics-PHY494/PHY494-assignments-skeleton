896ff1eb82d9d2f6e8e615a9b7286436699ae7d4
Oliver Beckstein <orbeckst@gmail.com>
2018-02-09 00:18:01 -0700

    HW04, due Feb 15, 2018, 11:59pm

---
+++ assignment_04/Submission/problem1.py
@@ -0,0 +1,27 @@
+# Problem 1
+
+import numpy as np
+
+sx = np.array([[0, 1], [1, 0]])
+sy = np.array([[0, -1j],[1j, 0]])
+sz = np.array([[1, 0], [0, -1]])
+
+# replace NotImplemented with your solution
+
+# b (i)
+result1b1 = NotImplemented
+
+
+# b (ii)
+result1b2 = NotImplemented
+
+
+# b (iii)
+result1b3 = NotImplemented
+
+
+# b (iv)
+v = NotImplemented
+result1b4 = NotImplemented
+
+
