b4ed153b181f5d263ab5d8ce8f6b62d3b8dca76f
Oliver Beckstein <orbeckst@gmail.com>
2018-04-20 00:10:57 -0700

    add testing for output files

--- Makeup_02/Submission/test_capacitor.py
+++ Makeup_02/Submission/test_capacitor.py
@@ -1,5 +1,6 @@
 # tests for Makeup 02 solution
 
+import glob
 import numpy as np
 
 from numpy.testing import (assert_array_almost_equal,
@@ -21,32 +22,6 @@ def test_import_function():
     except ImportError:
         raise AssertionError("Cannot import the capacitor.calculate_phi_capacitor function")
 
-
-@pytest.fixture(scope="module")
-def phi_long(Max_iter=10000, tol=1e-3):
-    import capacitor
-    Phi = capacitor.calculate_phi_capacitor(Max_iter=Max_iter, tol=tol)
-    return Phi
-
-class TestPhiLong(object):
-    def test_shape(self, phi_long):
-        Nx, Ny = phi_long.shape
-        assert_equal(phi_long.shape, (100, 100))
-
-    def test_min(self, phi_long):
-        assert_almost_equal(phi_long.min(), -100)
-
-    def test_max(self, phi_long):
-        assert_almost_equal(phi_long.max(), 100)
-
-    def test_diagonal_sum(self, phi_long):
-        assert_almost_equal(np.trace(phi_long),
-                            72.637059850488143, decimal=3)
-
-    def test_half_sum(self, phi_long):
-        Nx, Ny = phi_long.shape
-        assert_almost_equal(phi_long[:Nx//2, :].sum(),
-                            1127.5308751853702, decimal=3)
 
 @pytest.fixture(scope="module")
 def phi_short(Max_iter=1000, tol=1):
@@ -75,44 +50,83 @@ class TestPhiShort(object):
                             542.60037474563251, decimal=0)
 
 
+@pytest.fixture(scope="module")
+def phi_long(Max_iter=10000, tol=1e-3):
+    import capacitor
+    Phi = capacitor.calculate_phi_capacitor(Max_iter=Max_iter, tol=tol)
+    return Phi
+
+class TestPhiLong(object):
+    def test_shape(self, phi_long):
+        Nx, Ny = phi_long.shape
+        assert_equal(phi_long.shape, (100, 100))
+
+    def test_min(self, phi_long):
+        assert_almost_equal(phi_long.min(), -100)
+
+    def test_max(self, phi_long):
+        assert_almost_equal(phi_long.max(), 100)
+
+    def test_diagonal_sum(self, phi_long):
+        assert_almost_equal(np.trace(phi_long),
+                            72.637059850488143, decimal=3)
+
+    def test_half_sum(self, phi_long):
+        Nx, Ny = phi_long.shape
+        assert_almost_equal(phi_long[:Nx//2, :].sum(),
+                            1127.5308751853702, decimal=3)
+
+@pytest.fixture(scope="class")
+def Phi0(Nmax=100):
+    import capacitor
+
+    _Phi0 = np.zeros((Nmax, Nmax))
+    _Phi0 = capacitor.set_boundaries(_Phi0)
+    return _Phi0
+
 class TestBoundaryConditions(object):
-    def setup(self):
-        import capacitor
+    # correct values for the given problem
+    plate_size = 50
+    voltage = 100.
 
-        self.Nmax = 100
-        self.Phi0 = np.zeros((self.Nmax, self.Nmax))
-        self.Phi0 = capacitor.set_boundaries(self.Phi0)
-        # correct values for the given problem
-        self.plate_size = 50
-        self.voltage = 100.
+    def test_voltage(self, Phi0):
+        assert_almost_equal(Phi0.min(), -self.voltage)
+        assert_almost_equal(Phi0.max(), self.voltage)
 
-    def test_voltage(self):
-        assert_almost_equal(self.Phi0.min(), -self.voltage)
-        assert_almost_equal(self.Phi0.max(), self.voltage)
-
-    def test_plate_size(self):
-        plus_plate = self.Phi0 > 0
-        minus_plate = self.Phi0 < 0
+    def test_plate_size(self, Phi0):
+        plus_plate = Phi0 > 0
+        minus_plate = Phi0 < 0
         assert_equal(np.sum(plus_plate), self.plate_size)
         assert_equal(np.sum(minus_plate), self.plate_size)
 
-    def test_plus_plate_position(self):
-        x, y = np.where(self.Phi0 > 0)
+    def test_plus_plate_position(self, Phi0):
+        x, y = np.where(Phi0 > 0)
         assert_equal(x.min(), 25)
         assert_equal(y.min(), 40)
         assert_equal(x.max(), 74)
         assert_equal(y.max(), 40)
 
-    def test_minus_plate_position(self):
-        x, y = np.where(self.Phi0 < 0)
+    def test_minus_plate_position(self, Phi0):
+        x, y = np.where(Phi0 < 0)
         assert_equal(x.min(), 25)
         assert_equal(y.min(), 60)
         assert_equal(x.max(), 74)
         assert_equal(y.max(), 60)
 
-    def test_box_boundary(self):
-        assert_array_equal(self.Phi0[:, 0], 0)
-        assert_array_equal(self.Phi0[:, -1], 0)
-        assert_array_equal(self.Phi0[0, :], 0)
-        assert_array_equal(self.Phi0[-1, :], 0)
+    def test_box_boundary(self, Phi0):
+        assert_array_equal(Phi0[:, 0], 0)
+        assert_array_equal(Phi0[:, -1], 0)
+        assert_array_equal(Phi0[0, :], 0)
+        assert_array_equal(Phi0[-1, :], 0)
 
+
+
+
+@pytest.mark.parametrize('filename',
+                         ('iterations.txt',
+                          'capacitor_potential_2d.*',
+                          'capacitor_potential_3d.*')
+                         )
+def test_output_file_exists(filename):
+    filenames = glob.glob(filename)
+    assert len(filenames) >= 1, "File {} is missing".format(filename)
