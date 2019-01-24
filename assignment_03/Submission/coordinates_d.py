0487ae3c717e6ab3a0141489ac8777119030181f
Oliver Beckstein <orbeckst@gmail.com>
2019-01-24 16:09:04 -0700

    HW03 (submit through GitHub)
    Contains starter code and tests.

---
+++ assignment_03/Submission/coordinates_d.py
@@ -0,0 +1,29 @@
+# problem 3.4d
+
+# Write a Python function translate() to translate all particles by a vector t:
+# print the translated coordinates for t and t2
+
+positions = [[0.0, 0.0, 0.0],
+             [1.34234, 1.34234, 0.0],
+             [1.34234, 0.0, 1.34234],
+             [0.0, 1.34234, 1.34234]]
+
+positions2 = [[1.5, -1.5, 3],
+              [-1.5, -1.5, -3]]
+
+t = [1.34234, -1.34234, -1.34234]
+t2 = [-1.5, 1.5, 3]
+
+
+
+def translate(positions, t):
+    # complete the function
+
+
+print(translate(positions, t))
+print(translate(positions2, t2))
+
+
+
+
+
