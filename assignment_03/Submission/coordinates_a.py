309055a6cbafc6fd4ee8667493ab2c723e2f616b
Oliver Beckstein <orbeckst@gmail.com>
2020-01-31 10:43:58 -0700

    HW03 (due 2/6/2020)

---
+++ assignment_03/Submission/coordinates_a.py
@@ -0,0 +1,11 @@
+# problem 3.4a
+# print the coordinates of the second particle
+
+positions = [[0.0, 0.0, 0.0],
+             [1.34234, 1.34234, 0.0],
+             [1.34234, 0.0, 1.34234],
+             [0.0, 1.34234, 1.34234]]
+
+particle2 =
+
+print(particle2)
---
+++ assignment_03/Submission/coordinates_c.py
@@ -0,0 +1,15 @@
+# problem 3.4c
+
+# Write Python code to translate all particles by a vector t:
+# print the translated coordinates
+
+positions = [[0.0, 0.0, 0.0],
+             [1.34234, 1.34234, 0.0],
+             [1.34234, 0.0, 1.34234],
+             [0.0, 1.34234, 1.34234]]
+
+t = [1.34234, -1.34234, -1.34234]
+
+new_positions =
+
+print(new_positions)
