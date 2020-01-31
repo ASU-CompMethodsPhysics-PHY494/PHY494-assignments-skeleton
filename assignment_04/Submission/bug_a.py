fd3e7bcd629d35b7cce7f83945ac885303979104
Oliver Beckstein <orbeckst@gmail.com>
2020-01-31 10:45:19 -0700

    HW04 (due 2/13/2020)

---
+++ assignment_04/Submission/bug_a.py
@@ -0,0 +1,14 @@
+# Problem 4.3a
+# Fix the bug(s) so that the code runs correctly and submit the file.
+
+a, b, c = 45, 54, 455
+c, d. e = 55, 44, 554
+
+if a < d
+    value = "foo"
+elif c = b + 1
+    value = "bar"
+else
+    value = "boing"
+
+print(value)
